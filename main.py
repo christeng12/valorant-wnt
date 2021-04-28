495496fd6f89c3005421a1944045ce062f095db6
christeng12 <c.s.teng@wustl.edu>
2021-04-28 16:44:48 -0500

    linked homepage html

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -9,6 +9,6 @@ logging.basicConfig(level=logging.DEBUG)
 @app.route('/')
 def hello():
 	logging.debug("Saying Hello")
-	return "Welcome to WNT!"
+	return render_template("home.html")
 
 

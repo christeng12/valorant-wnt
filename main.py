6f2c5cc21be5333d02a226c997217eee08c46a78
christeng12 <c.s.teng@wustl.edu>
2021-04-29 21:17:57 -0500

    added linking

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -11,4 +11,7 @@ def hello():
 	logging.debug("Saying Hello")
 	return render_template("home.html")
 
-
+@app.route('/members')
+def members():
+	logging.debug("Displaying members")
+	return render_template("members.html")		

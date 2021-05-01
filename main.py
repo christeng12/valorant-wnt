ccd9bfef7c21801b80835d0ce467d174cba07183
christeng12 <c.s.teng@wustl.edu>
2021-04-30 22:09:26 -0500

    updated python version

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from riotwatcher import RiotWatcher, ValWatcher, ApiError
 #  Static variables 
 players = {"1":["WNT BigFella","WNT"]} # hardcoded, will retrieve dynamically from api later
 region = "na"
-#accWatcher = RiotWatcher(api_key)
-#valWatcher = ValWatcher(api_key)
+api_key = ""
+accWatcher = RiotWatcher(api_key)
+valWatcher = ValWatcher(api_key)
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -21,6 +22,6 @@ def hello():
 @app.route('/members')
 def members():
 	logging.debug("Displaying members")
-	#content = accWatcher.account.by_riot_id(region,players["1"][0],players["1"][1])
-	#logging.debug(content)
+	content = accWatcher.account.by_riot_id(region,players["1"][0],players["1"][1])
+	logging.debug(content)
 	return render_template("members.html")		
